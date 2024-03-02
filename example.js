5c7431b836f855aaa2d9dbb28bd1c9fe3e8d8c35
AkritW <mail2max.w@gmail.com>
2024-03-02 16:39:32 +0700

    switch to fp

--- example.js
+++ example.js
@@ -1,25 +1,27 @@
-const lineNotify = require('simple-line-notify')
+const initLineNotify = require("simple-line-notify");
 
 const main = async () => {
-    // insert API key
-    const lineCommunityGroup = new lineNotify('Bearer xxxxxxxxxxxxxxxxx')
-    const lineDevGroup = new lineNotify('Bearer xxxxxxxxxxxxxxxxxx')
+  // insert API key
+  const { sendMessage, sendSticker, sendImage } = initLineNotify(
+    "Bearer xxxxxxxxxxxxxxxxx",
+  );
 
-    // send message
-    const res1 = await lineDevGroup.send(JSON.stringify({ error: 'Uncaught TypeError: undefined is not a function' }))
-    const res2 = await lineCommunityGroup.send('Hello My Community')
+  // send message
+  const res1 = await sendMessage("Hello My Community");
 
-    // send sticker
-    const res3 = await lineCommunityGroup.sendSticker('Happy New Year 🎉', 446, 1992)
+  // send sticker
+  const res2 = await sendSticker("Happy New Year 🎉", 446, 1992);
 
-    // send image
-    const res4 = await lineCommunityGroup.sendImage('Here is a Cat picture!', 'https://i.ytimg.com/vi/fOd16PT1S7A/maxresdefault.jpg')
-    
-    // log
-    console.log(res1)
-    console.log(res2)
-    console.log(res3)
-    console.log(res4)
-}
+  // send image
+  const res3 = await sendImage(
+    "Here is a Cat picture!",
+    "https://i.ytimg.com/vi/fOd16PT1S7A/maxresdefault.jpg",
+  );
 
-main()
+  // log
+  console.log(res1);
+  console.log(res2);
+  console.log(res3);
+};
+
+main();
--- index.ts
+++ index.ts
@@ -1,59 +1,69 @@
-import fetch from 'node-fetch'
+import fetch from "node-fetch";
 
 interface HashMap {
-    [key: string]: any
+  [key: string]: any;
 }
 
 const parseToFormUrlEncoded = (jsonObj: HashMap): string => {
-    let formBody: string[] = []
-    for (const property in jsonObj) {
-        const encodedKey = encodeURIComponent(property);
-        const encodedValue = encodeURIComponent(jsonObj[property]);
-        formBody.push(encodedKey + "=" + encodedValue);
-    }
-    const stringifyFormBody = formBody.join("&");
+  let formBody: Array<string> = [];
+  for (const property in jsonObj) {
+    const encodedKey = encodeURIComponent(property);
+    const encodedValue = encodeURIComponent(jsonObj[property]);
+    formBody.push(encodedKey + "=" + encodedValue);
+  }
+  const stringifyFormBody = formBody.join("&");
 
-    return stringifyFormBody
-}
+  return stringifyFormBody;
+};
 
-class LineNotify {
-    private apiKey: string
-    constructor(apiKey: string) {
-        this.apiKey = apiKey
-    }
-    async send(msg: string) {
-        const res = await fetch('https://notify-api.line.me/api/notify', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': this.apiKey
-            },
-            body: parseToFormUrlEncoded({ message: msg })
-        })
-        return res;
-    }
-    async sendImage(msg: string, url: string) {
-        const res = await fetch('https://notify-api.line.me/api/notify', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': this.apiKey
-            },
-            body: parseToFormUrlEncoded({ message: msg, imageThumbnail: url, imageFullsize: url })
-        })
-        return res;
-    }
-    async sendSticker(msg: string, stickerPackageId: number, stickerId: number) {
-        const res = await fetch('https://notify-api.line.me/api/notify', {
-            method: 'POST',
-            headers: { 
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': this.apiKey
-            },
-            body: parseToFormUrlEncoded({ message: msg, stickerPackageId, stickerId })
-        })
-        return res;
-    }
-}
+const initLineApi = (apiKey: string) => {
+  return {
+    sendMessage: async (msg: string) => {
+      const res = await fetch("https://notify-api.line.me/api/notify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: apiKey,
+        },
+        body: parseToFormUrlEncoded({ message: msg }),
+      });
+      return res;
+    },
+    sendImage: async (msg: string, url: string) => {
+      const res = await fetch("https://notify-api.line.me/api/notify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: apiKey,
+        },
+        body: parseToFormUrlEncoded({
+          message: msg,
+          imageThumbnail: url,
+          imageFullsize: url,
+        }),
+      });
+      return res;
+    },
+    sendSticker: async (
+      msg: string,
+      stickerPackageId: number,
+      stickerId: number,
+    ) => {
+      const res = await fetch("https://notify-api.line.me/api/notify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: apiKey,
+        },
+        body: parseToFormUrlEncoded({
+          message: msg,
+          stickerPackageId,
+          stickerId,
+        }),
+      });
+      return res;
+    },
+  };
+};
 
-export = LineNotify;
+export = initLineApi;
